d639bf4859420acbdd5126dcd1aee66e086b1457
Ilias Rinis <iliasr@gmail.com>
2023-05-25 20:49:43 +0200

    Add script to update stats and publish README.md

--- Makefile
+++
@@ -1,3 +0,0 @@
-.PHONY:stats
-stats:
-	gh-stats all --template templates/README.md.tmpl --template-extras templates/README-extras.json --output README.md
---
+++ update-stats.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+main () {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -f|--force)
+        FORCE=1
+        shift
+        ;;
+      -p|--publish)
+        PUBLISH=1
+        shift
+        ;;
+      *)
+        echo "unknown flag: '$1'"
+        exit 1
+        ;;
+    esac
+  done
+
+  # if README.md is dirty, exit or discard changes if --force is used
+  if ! git diff --quiet README.md; then
+    if [ -n "$FORCE" ]; then
+      echo "README.md has changes; forcing discard"
+      git checkout README.md
+    else
+      echo "README.md has changes; abort"
+      exit 1
+    fi
+  fi
+
+  echo "generating stats in README.md"
+	gh-stats all --template templates/README.md.tmpl --template-extras templates/README-extras.json --output README.md
+
+  if git diff --quiet README.md; then
+    echo "no new stats calculated; README.md unchanged"
+    return
+  fi
+
+  echo "README.md updated"
+  [ -n "$PUBLISH" ] || return
+
+  git add README.md
+  git commit -m 'Update stats'
+  git push
+}
+
+main "$@"
